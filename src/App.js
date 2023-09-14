2a6a77c4f4cdf79c942c8f4930210915ba0c66c3
George Arbanas <george.arbanas@icloud.com>
2023-09-14 20:55:55 +0900

    added pet component and created a .js file

---
+++ src/App.js
@@ -0,0 +1,38 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed)
+  ]);
+};
+
+const App = () => {
+  return React.createElement(
+    "div",
+    {},
+    [
+      React.createElement("h1", {}, "Adopt Me!"),
+      React.createElement(Pet, {
+        animal: "cat",
+        name: "Ben",
+        breed: "Orange Tabby"
+      }),
+      React.createElement(Pet, {
+        animal: "Bird",
+        name: "Word",
+        breed: "Cockatiel"
+      }),
+      React.createElement(Pet, {
+        animal: "Dog",
+        name: "Luna",
+        breed: "Havanese"
+      })
+    ]
+  );
+};
+
+
+// let's go make something
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
