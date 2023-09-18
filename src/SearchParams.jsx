27e1f5bd6c855dc7e59deb41dedf615984b29370
George Arbanas <george.arbanas@icloud.com>
2023-09-18 10:10:46 +0900

    added sub-option breed

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"]
+const breeds = [];
 
 const SearchParams = () => {
   const {location, setLocation} = useState("");
   const [animal, setAnimal] = useState("")
+  const [breed, setBreed] = useState("")
 
   return (
     <div className="search-params">
@@ -19,11 +21,26 @@ const SearchParams = () => {
         </label>
         <label htmlFor='animal'>
           Animal
-          <select id="animal" value={animal} onChange={(e) => setAnimal(e.target.value)}>
+          <select
+          id="animal"
+          value={animal}
+          onChange={(e) => setAnimal(e.target.value)}>
             <option />
               {ANIMALS.map(animal =>
                 <option key={animal}>{animal}</option>
                 )}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select id="breed"
+          disabled={breeds.length === 0}
+          value={breed}
+          onChange={(e) => setBreed(e.target.value)}>
+            <option />
+            {breeds.map(breed =>
+              <option key={breed}>{breed}</option>
+              )}
           </select>
         </label>
       </form>
