2a9f4e1ecfe3780ce91c7c577c4e12ccc8a736a5
George Arbanas <george.arbanas@icloud.com>
2023-09-19 11:45:12 +0900

    bug fixes

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -4,7 +4,7 @@ import useBreedList from "./useBreedList.js";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
-  const { location, setLocation } = useState("");
+  const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
   const [pets, setPets] = useState([]);
