53e0eff46be4d712eaf4678071907773d0d794db
George Arbanas <george.arbanas@icloud.com>
2023-09-20 17:06:53 +0900

    created error boundary

--- src/Details.jsx
+++ src/Details.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import fetchPet from "./fetchPet";
 import Carousel from "./Carousel.jsx";
+import ErrorBoundary from "./ErrorBoundary";
 
 const Details = () => {
   const { id } = useParams();
@@ -32,4 +33,11 @@ const Details = () => {
   );
 };
 
-export default Details;
+function DetailsErrorBoundary() {
+  return (
+    <ErrorBoundary>
+      <Details />
+    </ErrorBoundary>
+  );
+}
+export default DetailsErrorBoundary;
