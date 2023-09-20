f6ddd24bd6817147609ffb1af5af72ae246c5e76
George Arbanas <george.arbanas@icloud.com>
2023-09-20 13:36:39 +0900

    added fetchPet react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
 import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
 import Details from "./Details.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
-  // return React.createElement("div", {}, [
-  //   React.createElement("h1", {}, "Adopt Me!"),
-  //   React.createElement(Pet, {
-  //     animal: "cat",
-  //     name: "Ben",
-  //     breed: "Orange Tabby",
-  //   }) old code
-
   return (
     <BrowserRouter>
-      <h1>Adopt Me!</h1>
-      <Routes>
-        <Route path="/details/:id" element={<Details />} />
-        <Route path="/" element={<SearchParams />}></Route>
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <h1>Adopt Me!</h1>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />}></Route>
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
